0a7080a5ab60e9405a1b9e7ee3a535b1c762bc6f
Markus Ånöstam <markus.anostam@x5music.com>
2018-12-21 18:11:37 +0100

    Add merge

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-const join      = c => xs => xs.join(c)
-const pipe      = f => g => x => g(f(x))
-const map       = f => xs => xs.map(f)
-const transpose = xsxs => xsxs[0].map((_, i) => xsxs.map(row => row[i]))
-const reverse   = xs => xs.reverse()
-const reflect   = map(reverse)
+const join = c => xs => xs.join(c);
+const pipe = f => g => x => g(f(x));
+const map = f => xs => xs.map(f);
+const transpose = xsxs => xsxs[0].map((_, i) => xsxs.map(row => row[i]));
+const reverse = xs => xs.reverse();
+const reflect = map(reverse);
+const merge = f => xs => ys => xs.map((_, i) => f(xs[i])(ys[i]));
+const add = x => y => x + y;
 
 const Piece = {
-  I: [[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],
-  O: [[2,2],[2,2]],
-  T: [[0,3,0],[3,3,3],[0,0,0]],
-  S: [[0,4,4],[4,4,0],[0,0,0]],
-  Z: [[5,5,0],[0,5,5],[0,0,0]],
-  J: [[6,0,0],[6,6,6],[0,0,0]],
-  L: [[0,0,7],[7,7,7],[0,0,0]],
-}
+  I: [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+  O: [[2, 2], [2, 2]],
+  T: [[0, 3, 0], [3, 3, 3], [0, 0, 0]],
+  S: [[0, 4, 4], [4, 4, 0], [0, 0, 0]],
+  Z: [[5, 5, 0], [0, 5, 5], [0, 0, 0]],
+  J: [[6, 0, 0], [6, 6, 6], [0, 0, 0]],
+  L: [[0, 0, 7], [7, 7, 7], [0, 0, 0]]
+};
 
-const M = {}
-M.toString = pipe(map(join('')))(join('\n'))
-M.rotate   = pipe(transpose)(reflect)
-
+const M = {};
+M.toString = pipe(map(join("")))(join("\n"));
+M.rotate = pipe(transpose)(reflect);
+M.merge = f => merge(merge(f));
